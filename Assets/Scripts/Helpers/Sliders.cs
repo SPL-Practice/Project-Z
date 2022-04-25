eed5b40c0627d6e840ca609664e0185aad513b66
Alexxx180 <alexx120702@gmail.com>
2022-04-25 13:15:58 +0300

    Player status and helpers created

---
+++ Assets/Scripts/Helpers/Sliders.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Helpers
+{
+    public static class Sliders
+    {
+        public static bool Drain(this Slider bar, int value)
+        {
+            bar.value = Mathf.Clamp(bar.value - value, bar.minValue, bar.maxValue);
+            return bar.value <= bar.minValue;
+        }
+
+        public static void Fill(this Slider bar, int value)
+        {
+            bar.value = Mathf.Clamp(bar.value + value, bar.minValue, bar.maxValue);
+        }
+    }
+}
