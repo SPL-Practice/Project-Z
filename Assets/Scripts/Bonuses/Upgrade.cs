f508058d26eb326d69ab81c43d127a9771c6c071
Alexxx180 <alexx120702@gmail.com>
2022-04-27 07:34:18 +0300

    Upgrades logic set

---
+++ Assets/Scripts/Bonuses/Upgrade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+
+    public float SpeedBoost = 15f;
+    public int CountBoost = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (other.GetComponent(nameof(Shooting)) is Shooting player)
+        {
+            player.SpeedUp(SpeedBoost);
+            player.CountUp(CountBoost);
+
+            Destroy(gameObject);
+        }
+
+    }
+}
