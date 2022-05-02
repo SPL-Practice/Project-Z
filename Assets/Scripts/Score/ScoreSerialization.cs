a7e55d640f03b2c63b8f2dffc28fed4dee71350e
SidOneTT <solovei2175@mail.ru>
2022-05-02 16:41:16 +0300

    Score and Serialization

---
+++ Assets/Scripts/Score/ScoreSerialization.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class ScoreSerialization : MonoBehaviour
+{
+    
+    public void SaveMode()
+    {
+
+        ScoreCount scoreForSaving = new ScoreCount();
+
+        BinaryFormatter SaveForm = new BinaryFormatter();
+
+        FileStream sw = new FileStream("Score.dat", FileMode.OpenOrCreate);
+
+        SaveForm.Serialize(sw,scoreForSaving);
+
+        sw.Close();
+    
+    }
+    
+    public void LoadMode()
+    {
+
+        BinaryFormatter LoadForm = new BinaryFormatter();
+
+        FileStream sr = new FileStream("Score.dat", FileMode.OpenOrCreate);
+
+        ScoreCount scoreForLoad = (ScoreCount)LoadForm.Deserialize(sr);
+        
+        sr.Close();
+    
+    }
+}
