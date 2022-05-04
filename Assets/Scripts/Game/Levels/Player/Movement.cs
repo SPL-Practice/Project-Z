41e4938aad01d1f409833da7aace4fbaeb66a0d2
Alexxx180 <alexx120702@gmail.com>
2022-05-04 23:58:43 +0300

    Screen scalers added

--- Assets/Scripts/Game/Levels/Player/Movement.cs
+++ Assets/Scripts/Game/Levels/Player/Movement.cs
@@ -6,7 +6,19 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        Move();
+        //Move();
+
+        //ClampMove();
+    }
+
+    // Original Movement
+    private void Move()
+    {
+        float horizont = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(horizont, vertical, 0);
+        transform.Translate(dir.normalized * Time.deltaTime * speed);
     }
 
     // Mobile touch capture
@@ -17,12 +29,32 @@ public class Movement : MonoBehaviour
     }
 
     // Keyboard. For testing
-    private void Move()
-    {
-        float horizont = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+    //private void ClampMove()
+    //{
+    //    float horizont = Input.GetAxisRaw("Horizontal");
+    //    float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 dir = new Vector3(horizont, vertical, 0);
-        transform.Translate(dir.normalized * Time.deltaTime * speed);
-    }
+    //    Vector3 dir = new Vector3(horizont, vertical, 0);
+    //    Vector3 normalization = dir.normalized * Time.deltaTime * speed;
+    //    Vector3 future = transform.position;
+    //    //Screen.height / 720
+    //    //Screen.width / 1280
+
+    //    float x = future.x + normalization.x;
+    //    float y = future.y + normalization.y;
+
+    //    float clampX = 8.25f * Screen.width / 1280;
+    //    float clampY = 4.5f * Screen.height / 720;
+
+    //    Debug.Log(x);
+    //    Debug.Log(y);
+
+    //    Debug.Log(clampX);
+    //    Debug.Log(clampY);
+    //    if (x > -1 * clampX && x < clampX
+    //         && y > -1 * clampY && y < clampY)
+    //    {
+    //        transform.Translate(normalization);
+    //    }
+    //}
 }
