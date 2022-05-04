b4d5dbac0b51648075df2ddf30e4ef787d5afa2d
Alexxx180 <alexx120702@gmail.com>
2022-05-05 00:52:11 +0300

    First level finished

--- Assets/Scripts/Game/Levels/Player/Status.cs
+++ Assets/Scripts/Game/Levels/Player/Status.cs
@@ -21,6 +21,24 @@ public class Status : MonoBehaviour, IWeak
 
     private void Defeat()
     {
+        gameObject.SetActive(false);
         // Some behaviour...
+    }
+
+    // On player collide
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+
+        if (!other.CompareTag("Enemy"))
+            return;
+
+        GameObject parent = other.transform.parent.gameObject;
+
+        if (parent.GetComponent(nameof(Obstacle)) is Obstacle obstacle)
+        {
+            Debug.Log("YES X4");
+            Hit(obstacle.power);
+        }
     }
 }
