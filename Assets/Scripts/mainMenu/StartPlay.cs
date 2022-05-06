47309571bcbb3e46db0ea03d5cdd2320bda4e704
centalcode <98169080+oneplayah@users.noreply.github.com>
2022-05-06 17:47:35 +0300

    MenuPlusArea

---
+++ Assets/Scripts/mainMenu/StartPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPlay : MonoBehaviour
+{
+    public void NextScense()
+    {
+        Invoke("Delay", 2.7f);
+    }
+
+
+    public void Delay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
