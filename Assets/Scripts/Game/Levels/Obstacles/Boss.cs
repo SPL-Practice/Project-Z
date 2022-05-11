be5324ba13f814af2ee520e4ac7eeddc72f3fff2
Alexxx180 <alexx120702@gmail.com>
2022-05-12 02:04:46 +0300

    Boss started...

---
+++ Assets/Scripts/Game/Levels/Obstacles/Boss.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Helpers;
+
+public class Boss : MonoBehaviour
+{
+    #region Health Members
+    public Slider hp;
+    [SerializeField] private int health = 500;
+
+    public void Hit(ushort value)
+    {
+        Debug.Log("Player ship hit");
+
+        if (hp.Drain(value))
+        {
+            Defeat();
+        }
+    }
+
+    private void Defeat()
+    {
+        Debug.Log("PINKY HAS BEEN DEFEATED");
+        Debug.Log("Epic Victory!!!");
+
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    #endregion
+
+    internal void OnEnable()
+    {
+        ResetLevel();
+    }
+
+    private void ResetLevel()
+    {
+        hp.maxValue = health;
+        hp.value = health;
+    }
+}
