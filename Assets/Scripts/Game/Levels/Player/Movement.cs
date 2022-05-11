a79b97468944c9d68814c1b0db2651a196935380
Alexxx180 <alexx120702@gmail.com>
2022-05-11 23:11:23 +0300

    Movement improved: edge colliders bug fixed

--- Assets/Scripts/Game/Levels/Player/Movement.cs
+++ Assets/Scripts/Game/Levels/Player/Movement.cs
@@ -2,11 +2,26 @@
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10f;
+    public Rigidbody2D body;
+
+
+    [Range(0, 0.3f)] [SerializeField] private float smoothing = 0.05f;
+    [Range(10, 100f)] [SerializeField] public float speed = 10f;
+
+    private Vector3 _velocity = Vector3.zero;
+    private float x;
+    private float y;
+    
+
+    private void Awake()
+    {
+        x = 0f;
+        y = 0f;
+    }
 
     void Update()
     {
-        Move();
+        AxisLooking();
     }
 
     // Original Movement
@@ -17,6 +32,27 @@ public class Movement : MonoBehaviour
 
         Vector3 dir = new Vector3(horizont, vertical, 0);
         transform.Translate(dir.normalized * Time.deltaTime * speed);
+    }
+
+    private void AxisLooking()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void ForceMove()
+    {
+        float delta = Time.fixedDeltaTime;
+        Vector3 targetVelocity = new Vector2
+            (x * delta * speed * 10f, y * delta * speed * 10f);
+
+        body.velocity = Vector3.SmoothDamp
+            (body.velocity, targetVelocity, ref _velocity, smoothing);
+    }
+
+    private void FixedUpdate()
+    {
+        ForceMove();
     }
 
     // Mobile touch capture
