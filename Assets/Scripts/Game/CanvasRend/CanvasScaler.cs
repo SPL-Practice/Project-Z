d87a6db1f299ac16d71872770088d8e0eecd64ba
centalcode <98169080+oneplayah@users.noreply.github.com>
2022-05-26 18:00:05 +0300

    OneVersion

---
+++ Assets/Scripts/Game/CanvasRend/CanvasScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CanvasScaler : MonoBehaviour
+{
+    private const float V = 0.5f;
+
+    public void Scaler()
+    {
+        float width;
+        float heigh;
+        
+
+
+        width = Screen.width;
+        heigh = Screen.height;
+
+        Camera.main.orthographicSize = V * heigh / 100f;
+        
+    }
+}
